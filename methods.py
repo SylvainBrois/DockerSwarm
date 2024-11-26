6cc1f9496ae74bd199edab7f407c6ae7a9953004
sylvainB <brois8572@gmail.com>
2024-11-26 09:50:41 +0100

    ajout connexion db

--- methods.py
+++ methods.py
@@ -22,7 +22,7 @@ def db_check_connection():
         if connection.is_connected():
             return "Connexion réussie à la base de données."
     except Error as e:
-        return f"Erreur lors de la connexion à la base de données : {e}"
+        return f"Erreur lors de la connexion à la base de données : {e} {os.getenv('MYSQL_HOST')} {os.getenv('MYSQL_PORT')}"
     finally:
         # Fermeture de la connexion si elle est ouverte
         if 'connection' in locals() and connection.is_connected():
