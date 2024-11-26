b4799031632f787ab56e77423e09fe0cf4f90dad
sylvainB <brois8572@gmail.com>
2024-11-26 08:53:56 +0100

    ptet ça marche

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():  
     return "Hello, Docker Swarm!"  
 
+@app.route('/test')
+def test():
+    return "test"
+
 if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5000)  
