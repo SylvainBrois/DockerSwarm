4b55f473c24cb810e8ceb32a543d1296b9ea270f
sylvainB <brois8572@gmail.com>
2024-11-26 11:12:13 +0100

    ajout connexion db

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ def check_db():
 
 @app.route('/result/<x>/<y>')
 def result(x, y):
-    return x + y
+    return int(x) + int(y)
 if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5000)  
